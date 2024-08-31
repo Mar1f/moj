4ca3d02fd26515651cd40cb23302a22d13e08ad0
Mar1f <938224402@qq.com>
2024-08-31 10:26:49 +0800

    解决无法切换语言的bug

--- moj-forntend/src/components/CodeEditor.vue
+++ moj-forntend/src/components/CodeEditor.vue
@@ -42,25 +42,17 @@ const codeEditor = ref();
 //   toRaw(codeEditor.value).setValue("新的值");
 // };
 
-// watch(
-//   () => props.language,
-//   () => {
-//     codeEditor.value = monaco.editor.create(codeEditorRef.value, {
-//       value: props.value,
-//       language: props.language,
-//       automaticLayout: true,
-//       colorDecorators: true,
-//       minimap: {
-//         enabled: true,
-//       },
-//       readOnly: false,
-//       theme: "vs-dark",
-//       // lineNumbers: "off",
-//       // roundedSelection: false,
-//       // scrollBeyondLastLine: false,
-//     });
-//   }
-// );
+watch(
+  () => props.language,
+  () => {
+    if (codeEditor.value) {
+      monaco.editor.setModelLanguage(
+        toRaw(codeEditor.value).getModel(),
+        props.language
+      );
+    }
+  }
+);
 
 onMounted(() => {
   if (!codeEditorRef.value) {
